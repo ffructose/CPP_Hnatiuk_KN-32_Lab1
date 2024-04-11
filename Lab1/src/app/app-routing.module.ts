9317d1140afd66529b0d3a994c9570efd05e61ea
Anna <losers2004@gmail.com>
2024-04-11 03:45:41 -0600

    Revert "Lab3"
    This reverts commit c70fdecb70743844e7161d428a1b52ea4f253d43.

--- Lab1/src/app/app-routing.module.ts
+++ Lab1/src/app/app-routing.module.ts
@@ -9,10 +9,6 @@ const routes: Routes = [
   {
     path: 'graph',
     loadChildren: () => import('./graph/graph.module').then( m => m.GraphPageModule)
-  },
-  {
-    path: 'file',
-    loadChildren: () => import('./file/file.module').then( m => m.FilePageModule)
   }
 ];
 @NgModule({
