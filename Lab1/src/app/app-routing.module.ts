295efc33f61f1abf01ba7d0444fff59b3936690d
Anna <losers2004@gmail.com>
2024-04-29 19:09:58 +0200

    Lab7

--- Lab1/src/app/app-routing.module.ts
+++ Lab1/src/app/app-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
   {
     path: 'interfacepage',
     loadChildren: () => import('./interfacepage/interfacepage.module').then( m => m.InterfacepagePageModule)
+  },
+  {
+    path: 'servicepage',
+    loadChildren: () => import('./servicepage/servicepage.module').then( m => m.ServicepagePageModule)
   }
 
 ];
