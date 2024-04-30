18a4a45016a4038d4d6f32396032542e418fc58b
Anna <losers2004@gmail.com>
2024-04-30 23:22:42 +0200

    Lab9

---
+++ Lab10/src/app/myform/viewform/viewform.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewform',
+  templateUrl: './viewform.component.html',
+  styleUrls: ['./viewform.component.scss'],
+})
+export class ViewformComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
